685e79f75a52edb2b9e9e711675d37a835e819ce
dai <daichi.iphone@icloud.com>
2022-03-18 17:19:28 +0900

    バグ修正

--- creature_quine_100.rb
+++ creature_quine_100.rb
@@ -1,4 +1,4 @@
-n=$0.gsub(/[^\d]/, "").to_i;eval(u = %q[File.write("quine#{n+1}.rb","n=$0.gsub(/[^"+92.chr+"d]/, #{34.chr+34.chr}).to_i;eval(u = %q["+ u +"])")])
+n=$0.gsub(/[^\d]/, "").to_i;eval(u = %q[File.write("creature_quine_#{n+1}.rb","n=$0.gsub(/[^"+92.chr+"d]/, #{34.chr+34.chr}).to_i;eval(u = %q["+ u +"])")])
 # 生物の定義1. 外界と膜で仕切られている。(ファイルで区切られている)
 # 生物の定義2. 代謝（物質やエネルギーの流れ）を行う。(数字の流れがある)
 # 生物の定義3. 自分の複製を作る。
