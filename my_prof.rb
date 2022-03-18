8004720aca7cd2e5f3a21087f9d175cc695c32f2
dai <daichi.iphone@icloud.com>
2022-03-18 13:41:18 +0900

    my first ascii progrum

---
+++ my_prof.rb
@@ -0,0 +1,11 @@
+
+
+eval(%w(
+    
+    pu     ts     ("my
+    "+   32       .c  hr
+    +" na         me    "+
+    32.           ch     r+
+    "i s"         +"    Ku
+    ni   to       mo  Da
+    ic     hi"    ))*"")
