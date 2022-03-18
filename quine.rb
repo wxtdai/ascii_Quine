0bd9f14f11a4082ff26edb4cee545599db9e40f6
dai <daichi.iphone@icloud.com>
2022-03-18 15:55:41 +0900

    many kind quines

--- quine.rb
+++ quine.rb
@@ -1,2 +1,3 @@
-
-s = %q[puts "s = %q[ " + s + " ]; eval s"]; eval s
+s = %q[puts "s = %q[" + s + "]; eval s"]; eval s
+eval(t = %q[puts "eval(t = %q["+t+"#])"])
+n=1;eval(u = %q[puts "n=#{n+1};eval(u = %q["+ u +"])"])
