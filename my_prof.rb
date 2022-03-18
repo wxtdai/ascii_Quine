810de52befc24162924a87ccb01e0ca826c21b00
dai <daichi.iphone@icloud.com>
2022-03-18 13:46:40 +0900

    綺麗にした

--- my_prof.rb
+++ my_prof.rb
@@ -1,11 +1,10 @@
-
-
 eval(%w(
     
-    pu     ts     ("my
-    "+   32       .c  hr
-    +" na         me    "+
-    32.           ch     r+
-    "i s"         +"    Ku
-    ni   to       mo  Da
-    ic     hi"    ))*"")
+    pu     ts       ("my
+    "+   32         .c   hr
+    +" na           me     "+
+    32.             ch      r+
+    "i s"           +3     2.
+    ch   r+         "K   un
+    it     om       oD ai
+    ch       i") )  *"")
